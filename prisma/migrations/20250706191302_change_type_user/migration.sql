19ac2dd9869686326802180d650820fac8ce4cb7
CB-Kvn <150515929+CB-Kvn@users.noreply.github.com>
2025-08-09 17:30:23 -0600

    .

--- prisma/migrations/20250706191302_change_type_user/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateEnum
-CREATE TYPE "UserTypes" AS ENUM ('BUYER', 'ADMIN');
-
--- AlterTable
-ALTER TABLE "Google" ADD COLUMN     "typeUser" "UserTypes" DEFAULT 'BUYER';
-
--- AlterTable
-ALTER TABLE "OrderItem" ADD COLUMN     "qtyDone" INTEGER;
